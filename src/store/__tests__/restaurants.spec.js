5f34e2c66518cc90f041cd7b207ecc1704d98a1d
Alessandro Vitali <alessandro.vitali@bit-eu.com>
2021-04-17 07:47:06 +0200

    Set up loading flag

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,28 +4,73 @@ import {loadRestaurants} from '../restaurants/actions';
 import restaurantReducer from '../restaurants/reducers';
 
 describe('restaurants', () => {
-  describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
-      const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
-      ];
-
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      };
-
-      const initialState = {records: []};
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const initialState = {};
 
       const store = createStore(
         restaurantReducer,
         initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(thunk),
       );
 
-      await store.dispatch(loadRestaurants());
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+  describe('loadRestaurants action', () => {
+    describe('when loading succeeds', () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+      let store;
 
-      expect(store.getState().records).toEqual(records);
+      /*
+       * Documented behavior:
+       * if we want Jest to wait for a promise in a beforeEach block
+       * to resolve, we should return it.
+       */
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => Promise.resolve(records),
+        };
+
+        const initialState = {records: []};
+
+        store = createStore(
+          restaurantReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        return store.dispatch(loadRestaurants());
+      });
+
+      it('stores the restaurants', () => {
+        expect(store.getState().records).toEqual(records);
+      });
+
+      it('clears the loading flag', () => {
+        expect(store.getState().loading).toEqual(false);
+      });
+    });
+
+    describe('while loading', () => {
+      it('sets a loading flag', () => {
+        const api = {loadRestaurants: () => new Promise(() => {})};
+
+        const initialState = {};
+
+        const store = createStore(
+          restaurantReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        store.dispatch(loadRestaurants());
+
+        expect(store.getState().loading).toEqual(true);
+      });
     });
   });
 });
